892002d9f08efa4d126df51f0d285566ae009a61
zhaoyong <834162364@qq.com>
2018-05-19 13:03:02 +0800

    Author ZhaoYong Date 2018/05/19

--- ProjectCC/src/hello.cc
+++ ProjectCC/src/hello.cc
@@ -1,99 +1,99 @@
-#include <iostream>
-using namespace std;
-//int main()
-//{
-//	char name[] = "john";
-//    cout<<"hello"<<endl;
-//    cout<<"Hi"<<name<<"!"<<endl;
-//    return 0;
-//}
-void disp(char str[])
-		{
-			cout<<"test start "<<str<<endl;
-		}
-
-		void test(const int i)
-		{
-			cout<<i<<endl;
-		}
-      int f1()
-      {
-    	  int n =5000;
-    	  return n;
-      }
-
-      int f2()
-      {
-    	  int n =1000;
-    	  return n;
-      }
-	int main()
-	{
-//	       unsigned int age;
-//	       char name [50];
-//	       cout << "please enter your name:" << endl;
-//	       cin  >> name;
-//	       cout << "please enter your age:" << endl;
-//	       cin >> age;
-//	       cout << "your name is:" << name << endl;
-//	       cout << "You were " << age -2 << " years old two years ago. \n";
-
-//       char ch;
-//       cin>>ch;
-//       if(ch == 'y')
-//    	   cout<<"good"<<endl;
-//       else
-//    	   cout<<"try again."<<endl;
-//          int i;
-//          i = 5;
-//          cout<<"i="<<i<<endl;
-//          i = 100;
-//          cout<<"i="<<i<<endl;
-//          return 0;
-//           int i;
-//           double j;
-//           cout<<"size of i "<<sizeof(i)<<endl;
-//           cout<<"size of int "<<sizeof(int)<<endl;
-//           cout<<"size of j "<<sizeof(j)<<endl;
-//           cout<<"size of double "<<sizeof(double)<<endl;
-//           cout<<"size of char is:"<<sizeof(char)
-//        		   <<endl;
-//           cout<<"size of unsigned char is:"<<sizeof(unsigned char)<<endl;
-//           cout<<"size of signed char is :"<<sizeof(signed char)<<endl;
-//           cout<<"size of unsigned int is:"<<sizeof(unsigned int)<<endl;
-//           cout<<"size of signed int is:"<<sizeof(signed int)<<endl;
-//           cout<<"size of short int is:"<<sizeof(short int )<<endl;
-//           return 0;
-
-
-//			const double pi = 3.14;
-//			int a=8,b=4,c=5;
-////			cout<<(a%b?b:c);
-//			cout<<a%b?b:c;
-//		unsigned short int sum;
-//		sum = 65535;
-//		cout<<"sum = "<<sum<<endl;
-//		cout<<"sum = "<<sum+1<<endl;
-//         int sum = 2147483647;
-//         cout<<"sum = "<<sum<<endl;
-//         sum+=sum;
-//         cout<<"sum ="<<sum<<endl;
-//		int i =65535;
-//		int j =65535;
-//		cout<<i*j/9<<endl;
-//		int a,b,c,d;
-//		(a = 1, b = a, c = a+b, d = c + 5) = 100;
-//		cout<<"c = "<<c<<endl;
-//		char message[] ="message";
-//		char start[] = "start";
-//		disp(message);
-//		disp(start);
-//		int j,k;
-//		j=f1();
-//		k=f2();
-//		cout<<"j ="<<j<<endl;
-//		cout<<"k ="<<k<<endl;
-
-			test(200);
-	}
-
+//#include <iostream>
+//using namespace std;
+////int main()
+////{
+////	char name[] = "john";
+////    cout<<"hello"<<endl;
+////    cout<<"Hi"<<name<<"!"<<endl;
+////    return 0;
+////}
+//void disp(char str[])
+//		{
+//			cout<<"test start "<<str<<endl;
+//		}
+//
+//		void test(const int i)
+//		{
+//			cout<<i<<endl;
+//		}
+//      int f1()
+//      {
+//    	  int n =5000;
+//    	  return n;
+//      }
+//
+//      int f2()
+//      {
+//    	  int n =1000;
+//    	  return n;
+//      }
+//int main(const char* cout)
+//	{
+////	       unsigned int age;
+////	       char name [50];
+////	       cout << "please enter your name:" << endl;
+////	       cin  >> name;
+////	       cout << "please enter your age:" << endl;
+////	       cin >> age;
+////	       cout << "your name is:" << name << endl;
+////	       cout << "You were " << age -2 << " years old two years ago. \n";
+//
+////       char ch;
+////       cin>>ch;
+////       if(ch == 'y')
+////    	   cout<<"good"<<endl;
+////       else
+////    	   cout<<"try again."<<endl;
+////          int i;
+////          i = 5;
+////          cout<<"i="<<i<<endl;
+////          i = 100;
+////          cout<<"i="<<i<<endl;
+////          return 0;
+////           int i;
+////           double j;
+////           cout<<"size of i "<<sizeof(i)<<endl;
+////           cout<<"size of int "<<sizeof(int)<<endl;
+////           cout<<"size of j "<<sizeof(j)<<endl;
+////           cout<<"size of double "<<sizeof(double)<<endl;
+////           cout<<"size of char is:"<<sizeof(char)
+////        		   <<endl;
+////           cout<<"size of unsigned char is:"<<sizeof(unsigned char)<<endl;
+////           cout<<"size of signed char is :"<<sizeof(signed char)<<endl;
+////           cout<<"size of unsigned int is:"<<sizeof(unsigned int)<<endl;
+////           cout<<"size of signed int is:"<<sizeof(signed int)<<endl;
+////           cout<<"size of short int is:"<<sizeof(short int )<<endl;
+////           return 0;
+//
+//
+////			const double pi = 3.14;
+////			int a=8,b=4,c=5;
+//////			cout<<(a%b?b:c);
+////			cout<<a%b?b:c;
+////		unsigned short int sum;
+////		sum = 65535;
+////		cout<<"sum = "<<sum<<endl;
+////		cout<<"sum = "<<sum+1<<endl;
+////         int sum = 2147483647;
+////         cout<<"sum = "<<sum<<endl;
+////         sum+=sum;
+////         cout<<"sum ="<<sum<<endl;
+////		int i =65535;
+////		int j =65535;
+////		cout<<i*j/9<<endl;
+////		int a,b,c,d;
+////		(a = 1, b = a, c = a+b, d = c + 5) = 100;
+////		cout<<"c = "<<c<<endl;
+////		char message[] ="message";
+////		char start[] = "start";
+////		disp(message);
+////		disp(start);
+////		int j,k;
+////		j=f1();
+////		k=f2();
+////		cout<<"j ="<<j<<endl;
+////		cout<<"k ="<<k<<endl;
+//
+//			test(200);
+//	}
+//
